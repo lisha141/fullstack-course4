b2077b83b47be0d25a2a4c288e7ca8ab5a46e444
lisha141 <63555109+lisha141@users.noreply.github.com>
2020-04-26 20:10:45 +0530

    Update script.js

--- assignments/assignment4/assignment4-solution-starter/harder/script.js
+++ assignments/assignment4/assignment4-solution-starter/harder/script.js
@@ -41,8 +41,9 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 // using the 'speak' method or either helloSpeaker's or byeSpeaker's
 // 'speak' method.
 // See Lecture 50, part 1
-for (/* fill in parts of the 'for' loop to loop over names array */) {
-
+for (var i=0; i<10; i++) {
+var f=names[i].charAt(0);
+  var l=f.toLowerCase();
   // STEP 11:
   // Retrieve the first letter of the current name in the loop.
   // Use the string object's 'charAt' function. Since we are looking for
@@ -57,9 +58,9 @@ for (/* fill in parts of the 'for' loop to loop over names array */) {
   // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
-  if (/* fill in condition here */) {
-    // byeSpeaker.xxxx
+  if (l === j) {
+    console.log("Good Bye "+names[i]);
   } else {
-    // helloSpeaker.xxxx
+    console.log("Hello "+names[i]);
   }
 }
